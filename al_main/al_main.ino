3a2f8c1513a96e6e796a7b4590d4c5e43d81e00b
UnknownVariety <aawhitt5@gmail.com>
2015-05-17 18:47:32 -0700

    Fixed buttonVal issue and added const
    Const for BUTTON_LEFT value

--- al_main/al_main.ino
+++ al_main/al_main.ino
@@ -8,6 +8,8 @@ int speedPin_M2 = 6;     //M2 Speed Control
 int directionPin_M1 = 4;     //M1 Direction Control
 int directionPin_M2 = 7;     //M1 Direction Control
 
+const int BUTTON_LEFT = 410;
+
 void setup(){ 
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
@@ -22,18 +24,15 @@ void loop(){
   int iAnReading;
   iAnReading=analogRead(0);
   Serial.println(iAnReading);
-  if (buttonVal != iAnReading) {
-    if (iAnReading == 410) {
-      lcd.setCursor(0,1);
-      lcd.print("enjoy!          ");
-    }
-    else {
-      lcd.setCursor(0,1);
-      lcd.print("Val: ");
-      lcd.print(iAnReading);
-      lcd.print("hi hi hi     ");
-    }
-    buttonVal = iAnReading;
+  if (iAnReading == BUTTON_LEFT) {
+    lcd.setCursor(0,1);
+    lcd.print("enjoy!          ");
+  }
+  else {
+    lcd.setCursor(0,1);
+    lcd.print("Val: ");
+    lcd.print(iAnReading);
+    lcd.print("hi hi hi     ");
   }
 
   /*
